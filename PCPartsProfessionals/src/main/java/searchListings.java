767800519e307eef5601ad5dce300a86175db5bf
mwebb3005 <90664746+mwebb3005@users.noreply.github.com>
2023-05-02 14:26:19 -0500

    Add files via upload

--- PCPartsProfessionals/src/main/java/searchListings.java
+++ PCPartsProfessionals/src/main/java/searchListings.java
@@ -28,91 +28,105 @@ public class searchListings extends HttpServlet implements Info {
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       String param = request.getParameter("searchParam");
       String search = request.getParameter("search");
-      List<listingsTable> listings = ListingUtil.listListings();
-      Iterator<listingsTable> iterator = listings.iterator();
-
-      response.setContentType("text/html");
-      PrintWriter out = response.getWriter();
+      displayData(response.getWriter(), param, search);
+      
+   }
+   
+   void displayData(PrintWriter out, String param, String search) {
       String title = "Listings";
-      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
-      out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h1 align=\"center\">" + title + "</h1>\n");
-      out.println("<table>");
-      out.println("<tr>");
-      out.println("<th>ID</th>");
-      out.println("<th>Model</th>");
-      out.println("<th>Price</th>");
-      out.println("<th>City</th>");
-      out.println("<th>State</th>");
-      out.println("<th>Type</th>");
-      out.println("<th>Brand</th>");
-      out.println("<th>Condition</th>");
-      out.println("<th>Date</th>");
-      out.println("<th>User</th>");
-      out.println("<th>Email</th>");
-      out.println("<th>Phone</th>");
-      out.println("</tr>");
+      String header = "Results";
+      String content = "<ul>";
       
-      while(iterator.hasNext()) {
-    	  listingsTable listing = iterator.next();
-    	  boolean printCurrentListing = false;
-    	  switch (param) {
-
-    	  	case "model":
-    	  		if (search.equals(listing.getModel())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
-    	  	case "price":
-    	  		if (search.equals(listing.getPrice())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
-    	  	case "city":
-    	  		if (search.equals(listing.getCity())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
-    	  	case "partType":
-    	  		if (search.equals(listing.getType())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
-    	  	case "brand":
-    	  		if (search.equals(listing.getBrand())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
-    	  	case "condition":
-    	  		if (search.equals(listing.getCondition())) {
-    	  			printCurrentListing = true;
-    	  		}
-    	  		break;
+      List<listingsTable> listListings = util.ListingUtil.listListings();
+      for (listingsTable listing : listListings) {
+    	  if (param.equals("model") && listing.getModel().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
     	  }
+    	  if (param.equals("price") && listing.getPrice().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
+    	  }
+    	  if (param.equals("city") && listing.getCity().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
+    	  }
+    	  if (param.equals("partType") && listing.getType().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
+    	  }
+    	  if (param.equals("brand") && listing.getBrand().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
+    	  }
+    	  if (param.equals("condition") && listing.getCondition().equals(search)) {
+    		  content += ("<li>" + listing.getId() + ", "
+    				  + listing.getType() + ", "
+    				  + listing.getBrand() + ", "
+    				  + listing.getModel() + ", "
+    				  + listing.getCondition() + ", "
+    				  + listing.getPrice() + ", "
+    				  + listing.getDate() + ", "
+    				  + listing.getUser() + ", "
+    				  + listing.getEmail() + ", "
+    				  + listing.getPhone() + ", "
+    				  + listing.getCity() + ", "
+    				  + listing.getState());
+    	  }
+    	  content += "</ul>";
     	  
-    	  if (printCurrentListing) {
-    		  out.println("<tr>");
-    		  out.println("<td>"+ listing.getType() +"</td>");
-    		  out.println("<td>"+ listing.getBrand() +"</td>");
-    		  out.println("<td>"+ listing.getModel() +"</td>");
-    		  out.println("<td>"+ listing.getCondition() +"</td>");
-    		  out.println("<td>"+ listing.getPrice() +"</td>");
-    		  out.println("<td>"+ listing.getDate() +"</td>");
-    		  out.println("<td>"+ listing.getUser() +"</td>");
-    		  out.println("<td>"+ listing.getEmail() +"</td>");
-    		  out.println("<td>"+ listing.getPhone() +"</td>");
-    		  out.println("<td>"+ listing.getCity() +"</td>");
-    		  out.println("<td>"+ listing.getState() +"</td>");
-    		  out.println("</tr>");
-    		  printCurrentListing = false;
-    	  }
+    	  out.println(util.Info.PrettyPrint(title, header, content));
       }
-      
-      out.println("</table>");
-      out.println("</body></html>");
    }
 
    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
